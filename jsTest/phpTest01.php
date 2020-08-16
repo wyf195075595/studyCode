298a74748ab5096c04425c16830d4e8b354d5d53
wyf <1950755953@qq.com>
2020-08-17 00:03:46 +0800

    js测试

---
+++ jsTest/phpTest01.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @Author: Hu
+ * @Date:   2020-05-17 16:38:22
+ * @Last Modified by:   Hu
+ * @Last Modified time: 2020-05-17 16:39:41
+ */
+<?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "我的第一段 PHP 脚本！";
+?>
+
+</body>
+</html>
