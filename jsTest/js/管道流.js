7f0582e2e9a294aa7bea016689944014414b4b10
wyf195075595 <60338824+wyf195075595@users.noreply.github.com>
2021-01-30 16:09:09 +0800

    Delete jsTest directory

--- jsTest/js/管道流.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs')
-
-
-var readStream = fs.createReadStream('./jsTest/bz1.jpg');//创建读取流
-
-var writeStream = fs.createWriteStream('./jsTest/bz01.zip');//创建写入流
-
-readStream.pipe(writeStream);
